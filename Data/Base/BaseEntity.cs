a4be751192329168cda362bc57e369fd78d70220
Nilay Patel <nilay.patel@trivenitechnologies.in>
2022-08-08 18:08:11 +0530

    added validation in actor,movie

--- Data/Base/BaseEntity.cs
+++ Data/Base/BaseEntity.cs
@@ -5,6 +5,7 @@ namespace FirstProject.Data.Base
     public class BaseEntity
     {   [Key]
         public int Id { get; set; }
+        [Required (ErrorMessage = "Name is required")]
         public string? Name { get; set; }
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -1,13 +1,19 @@
-﻿using FirstProject.Data.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using FirstProject.Data.Base;
 
 namespace FirstProject.Models
 {
-    public class Actor :BaseEntity
+    public class Actor : BaseEntity
     {
+        [Required (ErrorMessage = "Age is required")]
         public int Age { get; set; }
+        [Required]
         public DateTime DOB { get; set; }
+        [Required]
         public string? City { get; set; }
+        [Required]
         public bool Gender { get; set; }
+        [Required]
         public string? Hobbie { get; set; }
 
         //Relatioships
